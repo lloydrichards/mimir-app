510195615ca2db8b6e08ddfda480fb33a0febd49
Lloyd Richards <lloyd.d.richards@gmail.com>
2021-06-14 13:58:59 +0200

    Save Form Button

--- mobile-app/src/Screens/Plants/PlantFormScreen.tsx
+++ mobile-app/src/Screens/Plants/PlantFormScreen.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react';
 import {StyleSheet, View} from 'react-native';
-import {Text} from 'react-native-paper';
+import {Button, Text} from 'react-native-paper';
 import {createStackNavigator} from '@react-navigation/stack';
 import {RouteProp} from '@react-navigation/native';
 import {StackNavigationProp} from '@react-navigation/stack';
 import {PlantsNavProps, PlantsStackParamList} from 'src/Routes/plantStack';
 import Center from 'src/Components/Molecule-UI/Center';
 import PlantForm from 'src/Components/Organism-Forms/PlantForm';
+import { useLayoutEffect } from 'react';
 
 interface Props {
   route: RouteProp<PlantsStackParamList, 'PlantDetails'>;
@@ -14,6 +15,13 @@ interface Props {
 }
 
 export const PlantFormScreen = ({navigation,route}: PlantsNavProps<'PlantDetails'>) => {
+
+  useLayoutEffect(() => {
+  navigation.setOptions({
+    headerRight:()=><Button>Save</Button>
+  })  
+  }, [navigation])
+
   return (
     <Center>
       <PlantForm onComplete={(plant)=>navigation.navigate("PlantDetails",{plant})}/>
